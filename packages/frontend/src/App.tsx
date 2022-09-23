baae6688622f49a6e42c9c6e6f5dacca917372ad
zamircohen <zamir_cohen@hotmail.com>
2022-09-23 20:46:56 +0200

    Created test output for frontend

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TodoItem from '../../shared/src/todo-item'
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState<TodoItem>({
+    id: '123',
+    text: 'Städa kontoret',
+    timeStamp: new Date(),
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {todo.text}
       </header>
     </div>
   );
