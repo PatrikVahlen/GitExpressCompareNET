b5b9e2653b145ec45380f4f0063031d9f0a13710
zamircohen <zamir_cohen@hotmail.com>
2022-09-23 20:37:53 +0200

    Added shared folder and packages

---
+++ packages/backend/src/app.ts
@@ -0,0 +1,20 @@
+import express, { Application, json, Request, Response } from 'express'
+import cors from 'cors'
+import TodoItem from '../../shared/src/todo-item'
+
+const app: Application = express()
+app.use(cors())
+app.use(json())
+const port : number = parseInt(process.env.SERVER_PORT || '3001')
+
+app.get('/todos', (req: Request, res: Response<TodoItem>) => {
+  res.send({
+    id: '1',
+    text: 'Hello World',
+    timeStamp: new Date()
+  })
+})
+
+app.listen(port, function () {
+    console.log(`App is listening on port ${port}!`)
+})
