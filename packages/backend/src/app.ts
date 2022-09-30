26aa1fdf70ab61abfd13d0f9e36448d7e6b1499c
zamircohen <zamir_cohen@hotmail.com>
2022-09-30 10:30:16 +0200

    Changed to database

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -4,6 +4,7 @@ import TodoItem from '../../shared/src/todo-item'
 import crypto from 'crypto'
 import dotenv from 'dotenv'
 import {readFile, writeFile} from 'fs'
+import {loadAllTodoItems, saveTodoItem, setupMongoDb} from './db'
 
 dotenv.config()
 
@@ -11,6 +12,7 @@ const app: Application = express()
 app.use(cors())
 app.use(json())
 const port : number = parseInt(process.env.SERVER_PORT || '3001')
+const mongoURL : string = process.env.MONGO_URL || 'mongodb://localhost:27017/mytodos'
 
 const TODO_FILE = process.env.TODO_FILE || 'todos.json'
 let TODO_ITEMS: TodoItem[] = []
@@ -57,6 +59,13 @@ app.put("/todos/:id", (req: Request<{id: string}>, res: Response<TodoItem[]>) =>
   res.send(TODO_ITEMS)
 });
 
-app.listen(port, function () {
-    console.log(`App is listening on port ${port}!`)
+
+app.listen(port, async function () {
+  await setupMongoDb(mongoURL)
+  console.log(`App is listening on port ${port}`)
 })
+
+
+// app.listen(port, function () {
+//     console.log(`App is listening on port ${port}!`)
+// })
